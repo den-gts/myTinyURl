65b9258e4ec003806e6763dead660d69de9e494e
Denis.Epifanov <epifanov.denis@gmail.com>
2016-08-03 23:29:57 +0300

    Switch random word to counter

--- tinyurl/app.py
+++ tinyurl/app.py
@@ -2,22 +2,34 @@ from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler, Application, url
 import motor
 import string
-import random
+
 MONGO_HOST = '10.250.216.67'
 
 
 class GetTynyHandler(RequestHandler):
-    def initialize(self, count, db):
-        self.char_count = count
+    def initialize(self, db):
         self.db = db
 
+    @staticmethod
+    async def _generate_tiny(number):
+        pattern = string.digits + string.ascii_letters
+        result = ''
+        highest = number
+        while highest:
+            highest, lowest = divmod(highest, len(pattern))
+            result += pattern[lowest]
+        return result[::-1]
+
     async def get(self, uri):
-        word = [random.choice(string.ascii_letters+string.digits) for i in range(self.char_count)]
-        word = ''.join(word)  # TODO Check for duplicate at database ?
+        word = await self._generate_tiny(self.application.counter)
+        self.application.counter += 1
         tiny_url = '{proto}://{host}/{word}'.format(proto=self.request.protocol,
                                                     host=self.request.host,
                                                     word=word)
         doc = {'tiny': word, 'full': uri}
+        await self.db['settings'].update({'_id': 'counter'},
+                                         {'_id': 'counter', 'value': self.application.counter},
+                                         upsert=True)
         await self.db['links'].insert(doc)
         self.write(tiny_url.encode('utf-8'))  # TODO return formatted doc. etc html page or json response
 
@@ -38,19 +50,24 @@ class TinyUrlHandler(RequestHandler):
         del request['connection']  # TODO implement recursive convertion all nested object to dict
         await self.db['log'].insert(request)
 
-        result = await self.db['links'].find_one(query)   # TODO riase 404 error if result is empty
+        result = await self.db['links'].find_one(query)
+        if not result:
+            self.send_error(status_code=404)
         self.redirect(result['full'])
 
 
 def run_server():
     client = motor.motor_tornado.MotorClient(MONGO_HOST, 27017)
     database = client['myapp']
+
     app = Application([
-        url(r'/get_tiny/(.*)', GetTynyHandler, dict(count=6, db=database)),
+        url(r'/get_tiny/(.*)', GetTynyHandler, dict(db=database)),
         url(r'/api/.*', ApiHandler),
-        url(r'/(.{6})', TinyUrlHandler, dict(db=database))
+        url(r'/(.+)', TinyUrlHandler, dict(db=database))
     ])
-    app.links = {}
+    counter = IOLoop.instance().run_sync(lambda: database['settings'].find_one({'_id': 'counter'}))
+    counter = counter['value'] if counter else 1
+    app.counter = counter
     app.listen(8888)
     IOLoop.current().start()
 
