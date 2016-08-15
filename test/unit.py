e2c0653da41432d942138265c9a28e7d163e5c48
Denis.Epifanov <epifanov.denis@gmail.com>
2016-08-15 09:01:37 +0300

    add test_redirect

--- test/unit.py
+++ test/unit.py
@@ -9,15 +9,20 @@ class TinyGenerator(AsyncHTTPTestCase):
     def get_app(self):
 
         mock_db = {'settings': mock.Mock(),
-                   'links': mock.Mock()}
+                   'links': mock.Mock(),
+                   'log': mock.Mock()}
         future = tornado.gen.Future()
         future.set_result({'_id': 'counter', 'value': 1456})
+        redirect_future = tornado.gen.Future()
+        redirect_future.set_result({'tiny': 'http://localhost/nu',
+                                    'full': 'http://localhost/some/res/1/'})
         empty_future = tornado.gen.Future()
         empty_future.set_result(None)
         mock_db['settings'].find_one = mock.Mock(return_value=future)
         mock_db['settings'].update = mock.Mock(return_value=empty_future)
         mock_db['links'].insert = mock.Mock(return_value=empty_future)
-
+        mock_db['links'].find_one = mock.Mock(return_value=redirect_future)
+        mock_db['log'].insert = mock.Mock(return_value=empty_future)
         tinyurld.app.bootstrap('D:\coding\myTinyURl/tinyurld\settings.py')
         self.db = mock_db
         return tinyurld.app.make_app(mock_db)
@@ -35,3 +40,6 @@ class TinyGenerator(AsyncHTTPTestCase):
         self.assertEqual(response.body,
                          '<html><a href={0}>{0}</a></html>'.format('http://localhost:{}/nu'.format(port)).encode(
                              'utf-8'))
+
+    def test_redirect(self):
+        response = self.fetch('/nu')
