158c0001306ad8455f53f5732d5b15c895e20520
Denis Epifanov <Denis.Epifanov@acronis.com>
2016-08-23 14:50:31 +0300

    expand settings.py. Add comments for parameters.

--- tinyurld/settings.py
+++ tinyurld/settings.py
@@ -1,2 +1,19 @@
-mongo_host = 'ati-customer-reporting'
+## This file contain settings. This settings overided by command-line parameters.
+
+## host - Ip address for bind
+#host = "0.0.0.0"
+## application port
+#port = 8888
+## Autoreload application after change files
+#autoreload=False
+## Tornado Debug mode
+#debug=False
+## MongoDB host IP
+mongo_host = 'your_mongo_db_host'
+## MongoDB port
 mongo_port = 27017
+## MongoDB user
+#mongo_user =some_user
+## MongoDB user password
+#mongo_password='some_pass'
+
